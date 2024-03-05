35ced22d9502cd38849ea6a6c64923d85bb09548
Sai Teja Goud Nakka <saitejagoudnakka@gmail.com>
2024-03-04 21:55:20 -0600

    created layouts for the application

---
+++ app/src/main/java/com/example/finalandroidproject/BookingCarActivity.java
@@ -0,0 +1,14 @@
+package com.example.finalandroidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookingCarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking_car);
+    }
+}
