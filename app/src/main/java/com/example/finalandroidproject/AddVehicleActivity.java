3770986ebfefeb625ceb40a5706baba024210609
JammulaSriLaxmiSathwika <143139467+JammulaSriLaxmiSathwika@users.noreply.github.com>
2024-03-04 22:14:02 -0600

    created xml file and java class for add vehicle and booking car

--- app/src/main/java/com/example/finalandroidproject/AddVehicleActivity.java
+++ app/src/main/java/com/example/finalandroidproject/AddVehicleActivity.java
@@ -1,14 +1,40 @@
 package com.example.finalandroidproject;
 
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.finalandroidproject.R;
+import com.example.finalandroidproject.UserViewActivity;
 
 public class AddVehicleActivity extends AppCompatActivity {
-
+    private Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_vehicle);
+        clickListenHandler();
+        initComponents();
+    }
+    public void initComponents() {
+//Register Button
+        back = findViewById(R.id.back);
+    }
+    private void clickListenHandler() {
+        back = findViewById(R.id.back);
+//back Listener
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// Start the UserViewActivity
+                Intent intent = new Intent(AddVehicleActivity.this, UserViewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/finalandroidproject/BookingCarActivity.java
+++ app/src/main/java/com/example/finalandroidproject/BookingCarActivity.java
@@ -1,14 +1,32 @@
 package com.example.finalandroidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 public class BookingCarActivity extends AppCompatActivity {
-
+    private Button bookingCar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking_car);
+        initcompo();
+        clickListenHandler();
+    }
+    public void initcompo(){
+        bookingCar=findViewById(R.id.bookNow);
+    }
+    private void clickListenHandler() {
+        bookingCar=findViewById(R.id.bookNow);
+//booking Listener
+        bookingCar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// Start the UserViewActivity
+                Intent intent = new Intent(BookingCarActivity.this, PaymentActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
