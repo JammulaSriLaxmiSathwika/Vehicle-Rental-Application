33ffb9d222ed5e5fcb45d9abbc53e4077a8121fd
MANICHANDRA900 <143015834+MANICHANDRA900@users.noreply.github.com>
2024-03-04 22:24:34 -0600

    Created xml and java file for login and payment activity

--- app/src/main/java/com/example/finalandroidproject/LoginActivity.java
+++ app/src/main/java/com/example/finalandroidproject/LoginActivity.java
@@ -1,14 +1,58 @@
 package com.example.finalandroidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 public class LoginActivity extends AppCompatActivity {
-
+    private TextView register;
+    private TextView login;
+    private EditText email;
+    private EditText password;
+    private Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initComponents();
+        clickListenHandler();
+    }
+    public void initComponents() {
+//Register Button
+        register = findViewById(R.id.register);
+//Login Button
+        login = findViewById(R.id.login);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.login);
+    }
+    private void clickListenHandler() {
+//Register Button
+        register = findViewById(R.id.register);
+//Login Button
+        login = findViewById(R.id.login);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.login);
+//login Listener
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// Start the UserViewActivity
+                Intent intent = new Intent(LoginActivity.this, UserViewActivity.class);
+                startActivity(intent);
+            }
+        });
+//Register Listener
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/finalandroidproject/PaymentActivity.java
+++ app/src/main/java/com/example/finalandroidproject/PaymentActivity.java
@@ -1,14 +1,37 @@
 package com.example.finalandroidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.finalandroidproject.R;
+import com.example.finalandroidproject.UserViewActivity;
 
 public class PaymentActivity extends AppCompatActivity {
-
+    public Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+        initComponents();
+        clickListenHandler();
+    }
+    public void initComponents() {
+//Register Button
+        back = findViewById(R.id.back1);
+    }
+    private void clickListenHandler() {
+        back = findViewById(R.id.back1);
+//login Listener
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// Start the UserViewActivity
+                Intent intent = new Intent(PaymentActivity.this, UserViewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
