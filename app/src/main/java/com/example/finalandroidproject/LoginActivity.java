08b81ffe155d817be2862f4280ea080b035e89a6
Jammula Sri Laxmi Sathwika <143139467+JammulaSriLaxmiSathwika@users.noreply.github.com>
2024-03-28 11:00:22 -0500

    Update LoginActivity.java
    updated login activity and created register button

--- app/src/main/java/com/example/finalandroidproject/LoginActivity.java
+++ app/src/main/java/com/example/finalandroidproject/LoginActivity.java
@@ -1,18 +1,21 @@
-package com.example.finalandroidproject;
+package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
 public class LoginActivity extends AppCompatActivity {
     private TextView register;
     private TextView login;
     private EditText email;
     private EditText password;
-    private Button loginButton;
+   private Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,33 +23,36 @@ public class LoginActivity extends AppCompatActivity {
         initComponents();
         clickListenHandler();
     }
+
     public void initComponents() {
-//Register Button
+        //Register Button
         register = findViewById(R.id.register);
-//Login Button
+        //Login Button
         login = findViewById(R.id.login);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         loginButton = findViewById(R.id.login);
     }
     private void clickListenHandler() {
-//Register Button
+        //Register Button
         register = findViewById(R.id.register);
-//Login Button
+        //Login Button
         login = findViewById(R.id.login);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         loginButton = findViewById(R.id.login);
-//login Listener
+
+        //login Listener
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-// Start the UserViewActivity
+                // Start the UserViewActivity
                 Intent intent = new Intent(LoginActivity.this, UserViewActivity.class);
                 startActivity(intent);
             }
         });
-//Register Listener
+
+        //Register Listener
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,5 +60,7 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
+
 }
