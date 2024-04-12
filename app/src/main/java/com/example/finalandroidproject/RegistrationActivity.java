471c72462274c57f85766c0cbb6b5e939fbd5664
Sai Teja Goud Nakka <143042337+saitejagoudnakka@users.noreply.github.com>
2024-04-11 20:52:42 -0500

    Update RegistrationActivity.java
    error in project name, so updated

--- app/src/main/java/com/example/finalandroidproject/RegistrationActivity.java
+++ app/src/main/java/com/example/finalandroidproject/RegistrationActivity.java
@@ -1,39 +1,4 @@
-//package com.example.finalproject;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import android.content.Intent;
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.Button;
-//
-//public class RegistrationActivity extends AppCompatActivity {
-// private Button signup;
-//
-// @Override
-// protected void onCreate(Bundle savedInstanceState) {
-// super.onCreate(savedInstanceState);
-// setContentView(R.layout.activity_registration);
-// clickListenHandler();
-// init();
-// }
-// public void init(){
-// signup = findViewById(R.id.signUp);
-// }
-// private void clickListenHandler() {
-// signup = findViewById(R.id.signUp);
-// //login Listener
-// signup.setOnClickListener(new View.OnClickListener() {
-// @Override
-// public void onClick(View v) {
-// // Start the UserViewActivity
-// Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
-// startActivity(intent);
-// }
-// });
-// }
-//}
-package com.example.finalproject;
+package com.example.finalandroidproject;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
