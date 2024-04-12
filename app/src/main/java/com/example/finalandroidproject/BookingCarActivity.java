2626d16be792e8cbe290b06fb714aafab07baf02
Jammula Sri Laxmi Sathwika <143139467+JammulaSriLaxmiSathwika@users.noreply.github.com>
2024-04-11 19:19:26 -0500

    Update BookingCarActivity.java
    Created random generated photos to display.

--- app/src/main/java/com/example/finalandroidproject/BookingCarActivity.java
+++ app/src/main/java/com/example/finalandroidproject/BookingCarActivity.java
@@ -1,7 +1,5 @@
 package com.example.finalproject;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,54 +7,62 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.squareup.picasso.Picasso;
-
+import java.util.Random;
 public class BookingCarActivity extends AppCompatActivity {
-    private Button bookingCar;
-    private TextView carNameTextView;
-    private TextView tvmodel,tvkm;
-    private ImageView carImageView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_booking_car);
-        initcompo();
-        clickListenHandler();
-        Intent intent = getIntent();
-        if (intent != null) {
-            String carName = intent.getStringExtra("carName");
-            String carImage = intent.getStringExtra("carImage");
-            String model = intent.getStringExtra("Model");
-            String km = intent.getStringExtra("Milleage");
-            Log.d("KM77777777",km);
-            carNameTextView.setText(carName);
-            tvmodel.setText(model);
-            tvkm.setText(km);
-            Picasso.get().load(carImage).into(carImageView);
-        }
-    }
-
-    public void initcompo() {
-        tvmodel = findViewById(R.id.tv_model);
-        tvkm = findViewById(R.id.tv_km);
-        carNameTextView = findViewById(R.id.carNameTextView);
-        carImageView = findViewById(R.id.carImageView);
-
-        bookingCar = findViewById(R.id.bookNow);
-    }
-
-    private void clickListenHandler() {
-        bookingCar=findViewById(R.id.bookNow);
-        //booking Listener
-        bookingCar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Start the UserViewActivity
-                Intent intent = new Intent(BookingCarActivity.this, PaymentActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
+private Button bookingCar;
+private TextView carNameTextView;
+private TextView tvmodel,tvkm;
+private ImageView carImageView;
+private int[] carImages = {
+R.drawable.accord1,
+R.drawable.city1,
+R.drawable.civic1,
+R.drawable.elantra1,
+R.drawable.passat1,
+R.drawable.rapid1,
+R.drawable.rent,
+R.drawable.seltos1,
+R.drawable.sonata1,
+R.drawable.vento1
+};
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+setContentView(R.layout.activity_booking_car);
+initcompo();
+clickListenHandler();
+Intent intent = getIntent();
+if (intent != null) {
+String carName = intent.getStringExtra("carName");
+String carImage = intent.getStringExtra("carImage");
+String model = intent.getStringExtra("Model");
+String km = intent.getStringExtra("Milleage");
+carNameTextView.setText(carName);
+tvmodel.setText(model);
+tvkm.setText(km);
+// Generate a random index to select a random photo
+int randomIndex = new Random().nextInt(carImages.length);
+Picasso.get().load(carImages[randomIndex]).into(carImageView);
+}
+}
+public void initcompo() {
+tvmodel = findViewById(R.id.tv_model);
+tvkm = findViewById(R.id.tv_km);
+carNameTextView = findViewById(R.id.carNameTextView);
+carImageView = findViewById(R.id.carImageView);
+bookingCar = findViewById(R.id.bookNow);
+}
+private void clickListenHandler() {
+bookingCar=findViewById(R.id.bookNow);
+//booking Listener
+bookingCar.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+// Start the UserViewActivity
+Intent intent = new Intent(BookingCarActivity.this, PaymentActivity.class);
+startActivity(intent);
+}
+});
+}
 }
