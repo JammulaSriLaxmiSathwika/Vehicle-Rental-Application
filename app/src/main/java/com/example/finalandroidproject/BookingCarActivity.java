8e340bb772d0413c7494d650374d341b37bc95e2
Jammula Sri Laxmi Sathwika <143139467+JammulaSriLaxmiSathwika@users.noreply.github.com>
2024-04-11 20:45:01 -0500

    Update BookingCarActivity.java
    updated project name

--- app/src/main/java/com/example/finalandroidproject/BookingCarActivity.java
+++ app/src/main/java/com/example/finalandroidproject/BookingCarActivity.java
@@ -1,4 +1,4 @@
-package com.example.finalproject;
+package com.example.finalandroidproject;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
